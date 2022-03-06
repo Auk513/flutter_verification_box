c612194013ea95dcc615ca6dca4dc38881f5703e
auk <auk@littlelights.ai>
2022-03-06 18:48:24 +0800

    [fix] build failure

--- lib/src/verification_box.dart
+++ lib/src/verification_box.dart
@@ -194,7 +194,7 @@ class _VerificationBox extends State<VerificationBox> {
       autofocus: widget.autoFocus,
       inputFormatters: [
         FilteringTextInputFormatter(RegExp("[0-9]"), allow: true),
-        ]
+        ],
 //      高版本flutter出现报错，没有此类型，用Filtering替换即可
 //      WhitelistingTextInputFormatter(RegExp("[0-9]")),
       
